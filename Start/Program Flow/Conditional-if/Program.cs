0bbcc694c8c48960756e847e2f3c3db045daaf6c
NateEnsign <nate.ensign@awardco.com>
2025-04-30 11:51:26 -0600

    ran a ternary operator

--- Start/Program Flow/Conditional-if/Program.cs
+++ Start/Program Flow/Conditional-if/Program.cs
@@ -9,15 +9,15 @@ namespace Conditionals
             int theVal = 55;
 
             // TODO: if-else 
-            if (theVal == 50){
-                Console.WriteLine("theVal is 50");
-            } 
-            else if (theVal >= 51 && theVal <= 60) {
-                Console.WriteLine("theVal is between 51 and 60");
-            }
-             else {
-                Console.WriteLine("theVal is something else");
-            }
+            // if (theVal == 50){
+            //     Console.WriteLine("theVal is 50");
+            // } 
+            // else if (theVal >= 51 && theVal <= 60) {
+            //     Console.WriteLine("theVal is between 51 and 60");
+            // }
+            //  else {
+            //     Console.WriteLine("theVal is something else");
+            // }
 
 
             // -----------------------
@@ -34,6 +34,8 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
+
+            Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
 
         }
     }
