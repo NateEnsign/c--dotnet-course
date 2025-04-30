ad2af3d1170622f1d6ba53fae099f7854feaec70
NateEnsign <nate.ensign@awardco.com>
2025-04-30 11:47:45 -0600

    wrote some else if statements

--- Start/Program Flow/Conditional-if/Program.cs
+++ Start/Program Flow/Conditional-if/Program.cs
@@ -6,9 +6,18 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 55;
 
             // TODO: if-else 
+            if (theVal == 50){
+                Console.WriteLine("theVal is 50");
+            } 
+            else if (theVal >= 51 && theVal <= 60) {
+                Console.WriteLine("theVal is between 51 and 60");
+            }
+             else {
+                Console.WriteLine("theVal is something else");
+            }
 
 
             // -----------------------
